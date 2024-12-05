8e77ea9f28dbc1c708e3e2cbf7dd12dd4c3c3503
Oracle Public Cloud User <opc@gdp4j.subnet11281621.vcn11281621.oraclevcn.com>
2024-12-05 12:38:04 +0000

    cleanup for GDP4J

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -42,16 +42,13 @@ import java.sql.Statement;
 @Controller
 public class DemoController {
 
-    private final ImageModel imageModel;
-
     private final ChatLanguageModel chatLanguageModel;
 
     private final EmbeddingModel embeddingModel;
 
     private final EmbeddingStore<TextSegment> embeddingStore;
 
-    public DemoController(ImageModel imageModel, ChatLanguageModel chatLanguageModel, EmbeddingModel embeddingModel, EmbeddingStore<TextSegment> embeddingStore) {
-        this.imageModel = imageModel;
+    public DemoController(ChatLanguageModel chatLanguageModel, EmbeddingModel embeddingModel, EmbeddingStore<TextSegment> embeddingStore) {
         this.chatLanguageModel = chatLanguageModel;
         this.embeddingModel = embeddingModel;
         this.embeddingStore = embeddingStore;
@@ -63,47 +60,22 @@ public class DemoController {
     }
 
     @GetMapping("/1")
-    String createImage(Model model) {
-        String question = "A coffee mug in Paris, France";
-        if (imageModel instanceof AzureOpenAiImageModel) {
-            String answer = imageModel.generate(question).content().url().toString();
-            return getView(model, "1: image generation", question, answer);
-        } else {
-            return getView(model, "1: image generation", question, "Image generation is only supported by Azure OpenAI");
-        }
-    }
-
-    @GetMapping("/2")
     String getAnswer(Model model) {
         String question = "Who painted the Mona Lisa?";
         String answer = chatLanguageModel.generate(UserMessage.from(question)).content().text();
-        return getView(model, "2: simple question", question, answer);
+        return getView(model, "1: Simple question","Using the TinyLlama chat language model (through Ollama that's running locally).", question, answer);
     }
 
-    @GetMapping("/3")
-    String reasoning(Model model) {
-        String question = "Maria's father has 4 daughters: Spring, Autumn, Winter. What is the name of the fourth daughter?";
-        String answer = chatLanguageModel.generate(UserMessage.from(question)).content().text();
-        return getView(model, "3: Reasoning question", question, answer);
-    }
-
-    @GetMapping("/4")
+    @GetMapping("/2")
     String getAnswerWithSystemMessage(Model model) {
         SystemMessage systemMessage = SystemMessage.from("I answer questions in French, in 100 words or less.");
 
         String question = "Give an explanation on how the Mona Lisa was painted.";
         String answer = chatLanguageModel.generate(systemMessage, UserMessage.from(question)).content().text();
-        return getView(model, "4: advanced question", question, answer);
+        return getView(model, "2: Advanced question", "Using the TinyLlama chat language model (through Ollama that's running locally).", question, answer);
     }
 
-    @GetMapping("/5")
-    String getAnswerWithLocation(Model model) {
-        String question = "Where can you see this painting?";
-        String answer = chatLanguageModel.generate(UserMessage.from(question)).content().text();
-        return getView(model, "5: A question without memory", question, answer);
-    }
-
-    @GetMapping("/6")
+    @GetMapping("/3")
     String getAnswerUsingConversationChain(Model model) {
         String context = "Who painted the Mona Lisa?";
         String question = "Where can you see this painting?";
@@ -116,10 +88,10 @@ public class DemoController {
 
         chain.execute(context);
         String answer = chain.execute(question);
-        return getView(model, "6: A question with memory", question, answer);
+        return getView(model, "3: A question with memory", "Still using the TinyLlama chat language model but with context 'Who painted the Mona Lisa?' (through Ollama that's running locally).", question, answer);
     }
 
-    @GetMapping("/7")
+    @GetMapping("/4")
     String loadVectorDatabase(Model model) {
         String content1 = "banana";
         String content2 = "computer";
@@ -135,10 +107,10 @@ public class DemoController {
             Embedding embedding = embeddingModel.embed(content).content();
             embeddingStore.add(embedding, textSegment);
         }
-        return getView(model, "7: Simple data ingestion", "Ingesting data into the vector database", "OK");
+        return getView(model, "4: Simple data ingestion", "Using the all-MiniLM-L6-v2 sentence transfomer (embedding model) that maps sentences & paragraphs to a 384 dimensional dense vectors that are then stored in an Autonomous Database using the langchain4j Oracle Embedding Store.", "Ingesting data into the vector database", "OK");
     }
 
-    @GetMapping("/8")
+    @GetMapping("/5")
     String queryVectorDatabase(Model model) {
         String question = "fruit";
 
@@ -155,10 +127,10 @@ public class DemoController {
                 .map(match -> match.embedded().text())
                 .collect(Collectors.joining("\n"));
 
-        return getView(model, "8: Querying the vector database", question, answer);
+        return getView(model, "5: Querying the vector database", "Using the all-MiniLM-L6-v2 sentence transfomer (embedding model) to transform the question into an embedding and then query Autonomous Database using a VECTOR distance function.", question, answer);
     }
 
-    @GetMapping("/9")
+    @GetMapping("/6")
     String queryVectorDatabaseWithDetails(Model model) {
         String question = "fruit";
 
@@ -175,20 +147,11 @@ public class DemoController {
                 .map(match -> match.embedded().text() + " | " + Arrays.toString(match.embedding().vector()))
                 .collect(Collectors.joining("\n"));
 
-        return getView(model, "9: Getting the vectors from the vector database", question, answer);
+        return getView(model, "6: Getting the vectors from the vector database","Using the all-MiniLM-L6-v2 sentence transfomer (embedding model) to transform the question into an embedding and then query Autonomous Database using a VECTOR distance function.", question, answer);
     }
 
-    @GetMapping("/10")
+    @GetMapping("/7")
     String ingestNews(Model model) {
-        /* Document document = UrlDocumentLoader.load("https://www.microsoft.com/investor/reports/ar23/index.html", new TextDocumentParser());
-        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .documentTransformer(new HtmlToTextDocumentTransformer(".annual-report"))
-                .documentSplitter(DocumentSplitters.recursive(300, 30))
-                .embeddingModel(embeddingModel)
-                .embeddingStore(embeddingStore)
-                .build();
-        */
-        // Document document = UrlDocumentLoader.load("file:/home/opc/Oracle-2024-10-K.html", new TextDocumentParser());
         Document document = UrlDocumentLoader.load("file:/home/opc/demo/langchain4j/langchain4j-demo/oBacle-2024-10-K.html", new TextDocumentParser());
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
         .documentTransformer(new HtmlToTextDocumentTransformer())
@@ -197,16 +160,13 @@ public class DemoController {
         .embeddingStore(embeddingStore)
         .build();
         //https://www.sec.gov/Archives/edgar/data/1341439/000095017023028914/orcl-20230531.htm
-        
-
-        
 
         ingestor.ingest(document);
 
-        return getView(model, "10: Advanced data ingestion", "Ingesting news about oBacle into the Oracle 23ai vector database", "OK");
+        return getView(model, "7: Advanced data ingestion", "Ingesting private documents into the Autonomous Database (here a fake oBacle report)", "Ingesting news about oBacle into the Oracle 23ai vector database", "OK");
     }
 
-    @GetMapping("/11")
+    @GetMapping("/8")
     String rag(Model model) {
         String question = "How many people are employed by oBacle in the US in 2023?";
 
@@ -217,20 +177,21 @@ public class DemoController {
 
         String answer = assistant.chat(question);
 
-        return getView(model, "11: Retrieval-Augmented Generation (RAG)", question, answer);
+        return getView(model, "8: Retrieval-Augmented Generation (RAG)","Putting it all together: enhance the TinyLlama chat language model (LLM) with data from Autonomous Database (Embedding Store) through the all-MiniLM-L6-v2 sentence transfomer (Embedding Model)", question, answer);
     }
 
-    @GetMapping("/12")
+    @GetMapping("/9")
     String deleteEmbeddings(Model model) {
 
         OracleDataSource ds;
         try {
             ds = new oracle.jdbc.datasource.impl.OracleDataSource();
-            Properties prop = new Properties();
-            prop.setProperty("user",System.getenv("ORACLE_JDBC_USER"));
-            prop.setProperty("password",System.getenv("ORACLE_JDBC_PASSWORD"));
-            ds.setConnectionProperties(prop);
-            ds.setURL(System.getenv("ORACLE_JDBC_URL"));
+            // Properties prop = new Properties();
+            // prop.setProperty("user",System.getenv("ORACLE_JDBC_USER"));
+            // prop.setProperty("password",System.getenv("ORACLE_JDBC_PASSWORD"));
+            // ds.setConnectionProperties(prop);
+            // ds.setURL(System.getenv("ORACLE_JDBC_URL"));
+            ds.setURL("jdbc:oracle:thin:@config-file://jdbc_connection_properties.json");
             try (
                 Connection conn = ds.getConnection();
                 Statement stmt = conn.createStatement();
@@ -242,11 +203,12 @@ public class DemoController {
             e.printStackTrace();
         }
 
-        return getView(model, "12: Delete embeddings", "Delete embeddings from the vector database", "OK");
+        return getView(model, "9: Delete embeddings", "", "Delete embeddings from the vector database", "OK");
     }
 
-    private static String getView(Model model, String demoName, String question, String answer) {
+    private static String getView(Model model, String demoName, String demoDetails, String question, String answer) {
         model.addAttribute("demo", demoName);
+        model.addAttribute("demoDetails", demoDetails);
         model.addAttribute("question", question);
         model.addAttribute("answer", answer);
         return "demo";
